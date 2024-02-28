c624808987da4b83bb08efc6c7240d6b42bf8f60
Shaked Kaplan <shaked@unit.co>
2024-02-28 10:27:30 +0200

    process file list

--- CompareApp.scala
+++ CompareApp.scala
@@ -2,37 +2,56 @@
 import java.nio.file.{Paths, Files}
 import java.nio.charset.StandardCharsets
 object CompareApp extends App {
-  private def writeResult(result: Boolean, githubOutput: String): Unit = {
-    Files.write(Paths.get(githubOutput), s"result=${result}".getBytes(StandardCharsets.UTF_8))
+  private def writeToOutput(
+      key: String,
+      value: String,
+      githubOutput: String
+  ): Unit = {
+    Files.write(
+      Paths.get(githubOutput),
+      s"${key}=${value}".getBytes(StandardCharsets.UTF_8)
+    )
   }
-  // get path from environment variable
-  val oldFile = sys.env("INPUT_OLD_FILE")
 
-  // get path from environment variable
-  val newFile = sys.env("INPUT_NEW_FILE")
+  val filesList =
+    sys.env("INPUT_FILES").split(",").map(file => (file, file + ".prev"))
 
   // read environment variable GITHUB_OUTPUT
   val githubOutput = sys.env("GITHUB_OUTPUT")
+  val result = filesList.map {
+    case (oldFile, newFile) => {
+      val oldFileParsed = FileParser.fromPathToClassDef(oldFile)
+      val newFileParsed = FileParser.fromPathToClassDef(newFile)
 
-  val oldFileParsed = FileParser.fromPathToClassDef(oldFile)
-  val newFileParsed = FileParser.fromPathToClassDef(newFile)
+      val compared =
+        BreakingChangeDetector.detectBreakingChange(
+          oldFileParsed,
+          newFileParsed
+        )
 
-  val compared =
-    BreakingChangeDetector.detectBreakingChange(oldFileParsed, newFileParsed)
+      if (compared.find(_.isBreakingChange).isEmpty) {
+        val output = "No breaking change detected"
+        // write false to environment variable GITHUB_OUTPUT
+        (false, output, oldFile)
+      } else {
+        val output =
+          List(
+            "Breaking change detected",
+            compared.filter(_.isBreakingChange).map(_.toString)
+          ).mkString("\n")
+        (true, output, oldFile)
 
-  if (compared.find(_.isBreakingChange).isEmpty) {
-    val output = "No breaking change detected"
-    // write false to environment variable GITHUB_OUTPUT
-    writeResult(false, githubOutput)
-    println(output)
-  } else {
-    val output =
-      List(
-        "Breaking change detected",
-        compared.filter(_.isBreakingChange).map(_.toString)
-      ).mkString("\n")
-    writeResult(true, githubOutput)
-    println(output)
+      }
     }
-    System.exit(0)
+  }
+  val finalResult = result.map(_._1).reduce(_ || _)
+  val finalOutput = result
+    .map { case (_, output, file) =>
+      s"File: $file\n$output"
+    }
+    .mkString("\n")
+
+  writeToOutput("result", finalResult.toString, githubOutput)
+  writeToOutput("log", finalResult.toString, githubOutput)
+  System.exit(0)
 }
